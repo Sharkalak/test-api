06479a0edd419672cbbc5f7fd46ad5bb851b7663
Sharkalak <97862252+Sharkalak@users.noreply.github.com>
2025-02-26 13:48:54 +0800

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+from CoolProp.CoolProp import PhaseSI, PropsSI, get_global_param_string
+
+app = FastAPI()
+
+
+@app.get("/demo/")
+def demo(Q: int,P: int):
+  
+    return PropsSI('T','P',P,'Q',Q,'Water')
+if __name__ == '__main__':
+    uvicorn.run(app=app, host="127.0.0.1", port=8080)
