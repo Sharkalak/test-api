8550e831d80bc551f6d56e0fde5120317673f282
Sharkalak <97862252+Sharkalak@users.noreply.github.com>
2025-02-26 15:31:21 +0800

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -8,6 +8,6 @@ app = FastAPI()
 @app.get("/demo/")
 def demo(Q: int,P: int):
   
-    return PropsSI('T','P',P,'Q',Q,'Water')
+    return PropsSI('T','P',101325,'Q',0,'Water')
 if __name__ == '__main__':
     uvicorn.run(app=app, host="127.0.0.1", port=8080)
